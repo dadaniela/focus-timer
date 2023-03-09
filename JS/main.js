a6d1db8394cd1677219667b4e3054c5adafeae40
Daniela <trindadedv@gmail.com>
2023-03-09 16:49:26 +0100

    adding events to buttons

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,23 @@
-let play = document.querySelector(".play");
-let pause = document.querySelector(".pause");
+const btnPlay = document.querySelector(".play");
+const btnPause = document.querySelector(".pause");
+const btnStop = document.querySelector(".stop");
+const btnSet = document.querySelector(".set");
 
-play.addEventListener("click", function() {
-    play.classList.add("hide");
-    pause.classList.remove("hide");
+btnPlay.addEventListener("click", function() {
+    btnPlay.classList.add("hide")
+    btnPause.classList.remove("hide")
+    btnSet.classList.add("hide")
+    btnStop.classList.remove("hide")
 })
 
-pause.addEventListener("click", function() {
-    pause.classList.add("hide");
-    play.classList.remove("hide");
+btnPause.addEventListener("click", function() {
+    btnPause.classList.add("hide")
+    btnPlay.classList.remove("hide")
+})
+
+btnStop.addEventListener("click", function() {
+    btnPlay.classList.remove("hide")
+    btnPause.classList.add("hide")
+    btnStop.classList.add("hide")
+    btnSet.classList.remove("hide")
 })
