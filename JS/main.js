97ce92a201d8e87266626ef64ba9ce5f99147a70
Daniela <trindadedv@gmail.com>
2023-03-10 16:24:01 +0100

    fixes if on line 30 to resulve the issue in the last round of the seconds

--- JS/main.js
+++ JS/main.js
@@ -4,26 +4,47 @@ const btnStop = document.querySelector(".stop");
 const btnSet = document.querySelector(".set");
 const btnMuted = document.querySelector(".muted");
 const btnUnmuted = document.querySelector(".unmuted");
-let minutes;
 const minutesDisplay = document.querySelector(".minutes");
 const secondsDisplay = document.querySelector(".seconds");
+let minutes;
+
+function resetControls() {
+    btnPlay.classList.remove("hide")
+    btnPause.classList.add("hide")
+    btnStop.classList.add("hide")
+    btnSet.classList.remove("hide")
+}
+
+function updateTimerDisplay(minutes, seconds) {
+    minutesDisplay.textContent = String(minutes).padStart(2, "0")
+    secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
 
 function countdown() {
     setTimeout(function() {
-        let seconds = Number(secondsDisplay.textContent)
-        let minutes = Number(minutesDisplay.textContent)
-        if(seconds <= 0) {
-            seconds = 3
-            minutesDisplay.textContent = String(minutes -1).padStart(2, "0")
-        }
-        
-        secondsDisplay.textContent = String(seconds -1).padStart(2, "0")
-        
-        if(minutes <= 0){
+        let currentSeconds = Number(secondsDisplay.textContent)
+        let currentMinutes = Number(minutesDisplay.textContent)
+
+        updateTimerDisplay(currentMinutes, 0)
+
+        if(currentMinutes <= 0 && currentSeconds <= 0) {
+            resetControls()
             return
         }
 
-        countdown()
+        if(currentSeconds <= 0) {
+            currentSeconds = 5
+            --currentMinutes
+            console.log(currentMinutes + " min")
+            console.log(currentSeconds + " sec")
+            // minutesDisplay.textContent = String(currentMinutes - 1).padStart(2, "0")
+            updateTimerDisplay(String(currentMinutes -1), currentSeconds)
+        }
+
+        // secondsDisplay.textContent = String(currentSeconds - 1).padStart(2, "0")
+        updateTimerDisplay(currentMinutes, String(currentSeconds -1))
+
+        countdown();
     }, 1000)
 }
 
@@ -41,11 +62,7 @@ btnPause.addEventListener("click", function() {
 })
 
 btnStop.addEventListener("click", function() {
-    btnPlay.classList.remove("hide")
-    btnPause.classList.add("hide")
-    btnStop.classList.add("hide")
-    btnSet.classList.remove("hide")
-    secondsDisplay.textContent = "00"
+    resetControls()
 })
 
 btnMuted.addEventListener("click", function() {
@@ -60,5 +77,6 @@ btnUnmuted.addEventListener("click", function() {
 
 btnSet.addEventListener("click", function() {
     minutes = prompt(`For how many minutes you'd like to focus?`)
-    minutesDisplay.textContent = String(minutes).padStart(2, "0")
+    console.log(minutes + " do prompt")
+    updateTimerDisplay(minutes, 0)
 })
